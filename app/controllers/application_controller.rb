3333e815c8af741d8d85a2629a5aba030f235c7e
Aida Abdikulova <abdikulova@gmail.com>
2020-03-07 05:37:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
         redirect "/failure"
     elsif
         params[:username] != "" && params[:password] != ""
-        user = User.new(:username => params[:username], :password => params[:password])
+        user = User.new(:username => params[:username], :password => params[:password], :balance => 0)
         user.save
         redirect "/login"
       end
